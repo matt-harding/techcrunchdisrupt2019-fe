7011a201addf0ac4722b44567a80a092cf462191
Matt Harding <matt.harding@thomsonreuters.com>
2019-10-03 13:31:55 -0700

    Added stabilize function

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -1,12 +1,28 @@
 <template>
-<div>
+  <div style="height: calc(100vh - 134px);">
+    <nav class="level">
+      <!-- Left side -->
+      <div class="level-left"></div>
+
+      <!-- Right side -->
+      <div class="level-right">
+        <p class="level-item">
+          <button class="button button-secondary" v-on:click="stabilizeGraph()">
+                <span class="icon">
+      <i class="material-icons">grain</i>
+    </span>
+    <span>Stabilize</span>
+          </button>
+        </p>
+      </div>
+    </nav>
     <div id="viz"></div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-const NeoVis = require('neovis.js');
+import { Component, Vue } from "vue-property-decorator";
+const NeoVis = require("neovis.js");
 
 @Component({
   components: {}
@@ -15,14 +31,14 @@ export default class Graph extends Vue {
   public viz: any;
   public draw() {
     const config = {
-      container_id: 'viz',
-      server_url: 'bolt://localhost:7687',
-      server_user: 'neo4j',
-      server_password: 'techcrunch',
+      container_id: "viz",
+      server_url: "bolt://localhost:7687",
+      server_user: "neo4j",
+      server_password: "techcrunch",
       labels: {
         Character: {
-          caption: 'id',
-          size: 'centrality'
+          caption: "id",
+          size: "centrality"
         }
       },
       // relationships: {
@@ -31,15 +47,19 @@ export default class Graph extends Vue {
       //     caption: false
       //   }
       // },
-      initial_cypher: 'MATCH (n)-[r:LINK]->(m) RETURN n,r,m'
+      initial_cypher: "MATCH (n)-[r:LINK]->(m) RETURN n,r,m"
     };
     this.viz = new NeoVis.default(config);
-    this.viz.registerOnEvent('completed', this.render_completed);
+    this.viz.registerOnEvent("completed", this.render_completed);
     this.viz.render();
   }
 
   public render_completed(stats: any) {
-    this.$store.commit('setNodes', this.viz._nodes);
+    this.$store.commit("setNodes", this.viz._nodes);
+  }
+
+  stabilizeGraph() {
+    this.viz.stabilize();
   }
 
   get nodes() {
@@ -52,8 +72,15 @@ export default class Graph extends Vue {
 }
 </script>
 <style lang="scss">
-  #viz {
-    border: 2px solid #dcdcdc;
-    max-height: calc(100vh - 150px);
-  }
+#viz {
+  border: 2px solid #dcdcdc;
+  max-height: calc(100vh - 150px);
+}
+
+.level {
+  background-color: white;
+  padding: 0;
+  margin-top: 10px !important;
+  margin-bottom: 10px !important;
+}
 </style>
